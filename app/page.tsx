1c9aa46db20ee6b7bacba050b96a6b6072e307fd
Bridger Tower <bridger@ziondesign.io>
2023-03-27 16:19:21 -0600

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from './page.module.css';
+import Form from '../components/form';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
 	return (
-		<main className="flex p-6">
-			<h1 className="text-6xl text-green-700">Welcome to Feedbackr</h1>
-			<div>
-				<h2 className="text-2xl">
-					The future of in-person feedback. Make more sales, keep more customers, and gather
-					valuable feedback.
-				</h2>
-				<button className="py-2 px-4 rounded-lg mt-4 bg-green-700 text-white">Coming Soon</button>
-			</div>
+		<main className="p-6 flex justify-center items-center w-screen h-screen">
+			<Form />
 		</main>
 	);
 }
